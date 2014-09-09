3fe7c3daa9107ca7916cbd0498f08fc8574fd863
Endy Muhardin <endy.muhardin@gmail.com>
2014-09-09 16:00:03 +0700

    implement select all dan hapus

--- aplikasi-training-management/src/main/java/com/artivisi/training/dao/PesertaDao.java
+++ aplikasi-training-management/src/main/java/com/artivisi/training/dao/PesertaDao.java
@@ -15,26 +15,32 @@ public class PesertaDao {
     // private static final String SQL_INSERT
     //        = "insert into peserta (id,kode, nama, email, tanggal_bergabung) "
     //        + "values(peserta_id_seq.nextval,?,?,?,?)";
-    
+
     // MySQL tidak pakai sequence
     private static final String SQL_INSERT
             = "insert into peserta (kode, nama, email, tanggal_bergabung) "
             + "values(?,?,?,?)";
-    
+
     private static final String SQL_UPDATE
             = "update peserta set kode = ?,"
             + "nama = ?, email = ?,"
             + "tanggal_bergabung = ? "
             + "where id = ?";
-    
+
     private static final String SQL_SELECT_BY_ID
             = "select * from peserta where id = ?";
     
-    public void simpan(Peserta p){
+    private static final String SQL_DELETE_BY_ID
+            = "delete from peserta where id = ?";
+
+    private static final String SQL_SELECT_ALL
+            = "select * from peserta";
+
+    public void simpan(Peserta p) {
         try {
             Connection c = KoneksiHelper.bukaKoneksi();
-            
-            if(p.getId() == null) {
+
+            if (p.getId() == null) {
                 PreparedStatement ps = c.prepareStatement(SQL_INSERT);
                 ps.setString(1, p.getKode());
                 ps.setString(2, p.getNama());
@@ -50,45 +56,74 @@ public class PesertaDao {
                 ps.setInt(5, p.getId());
                 ps.executeUpdate();
             }
+
+            KoneksiHelper.tutupKoneksi(c);
+        } catch (SQLException ex) {
+            Logger.getLogger(PesertaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void hapus(Peserta p) {
+        try {
+            Connection c = KoneksiHelper.bukaKoneksi();
+            PreparedStatement ps = c.prepareStatement(SQL_DELETE_BY_ID);
+            ps.setInt(1, p.getId());
+            ps.executeUpdate();
             
             KoneksiHelper.tutupKoneksi(c);
         } catch (SQLException ex) {
             Logger.getLogger(PesertaDao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void hapus(Peserta p){
-        
-    }
-    
-    public Peserta cariById(Integer id){
+
+    public Peserta cariById(Integer id) {
         Peserta p = null;
         try {
             Connection c = KoneksiHelper.bukaKoneksi();
-            
+
             PreparedStatement ps = c.prepareStatement(SQL_SELECT_BY_ID);
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                p = new Peserta();
-                p.setId(rs.getInt("id"));
-                p.setKode(rs.getString("kode"));
-                p.setNama(rs.getString("nama"));
-                p.setEmail(rs.getString("email"));
-                p.setTanggalBergabung(rs.getDate("tanggal_bergabung"));
+            if (rs.next()) {
+                p = konversiResultSetKePeserta(rs);
             }
-            
+
             KoneksiHelper.tutupKoneksi(c);
         } catch (SQLException ex) {
             Logger.getLogger(PesertaDao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return p;
     }
-    
-    public List<Peserta> cariSemua(){
-        List<Peserta> hasil = new ArrayList<Peserta>();
-        
-        
-        return hasil;
+
+    public List<Peserta> cariSemua() {
+        try {
+            List<Peserta> hasil = new ArrayList<Peserta>();
+            
+            Connection c = KoneksiHelper.bukaKoneksi();
+            
+            PreparedStatement ps = c.prepareStatement(SQL_SELECT_ALL);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Peserta p = konversiResultSetKePeserta(rs);
+                hasil.add(p);
+            }
+            
+            KoneksiHelper.tutupKoneksi(c);
+            
+            return hasil;
+        } catch (SQLException ex) {
+            Logger.getLogger(PesertaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    private Peserta konversiResultSetKePeserta(ResultSet rs) throws SQLException {
+        Peserta p = new Peserta();
+        p.setId(rs.getInt("id"));
+        p.setKode(rs.getString("kode"));
+        p.setNama(rs.getString("nama"));
+        p.setEmail(rs.getString("email"));
+        p.setTanggalBergabung(rs.getDate("tanggal_bergabung"));
+        return p;
     }
 }
--- aplikasi-training-management/src/main/java/com/artivisi/training/demo/PesertaDaoDemo.java
+++ aplikasi-training-management/src/main/java/com/artivisi/training/demo/PesertaDaoDemo.java
@@ -3,11 +3,12 @@ package com.artivisi.training.demo;
 import com.artivisi.training.dao.PesertaDao;
 import com.artivisi.training.domain.Peserta;
 import java.util.Date;
+import java.util.List;
 
 public class PesertaDaoDemo {
     public static void main(String[] args) {
         Peserta p = new Peserta();
-        p.setId(1);
+        //p.setId(1);
         p.setKode("P-001x");
         p.setNama("Peserta 001x");
         p.setEmail("p1x@gmail.com");
@@ -17,6 +18,17 @@ public class PesertaDaoDemo {
         pd.simpan(p);
         
         Peserta px = pd.cariById(1);
+        tampilkanPeserta(px);
+        
+        List<Peserta> semuaPeserta = pd.cariSemua();
+        for (Peserta pz : semuaPeserta) {
+            tampilkanPeserta(pz);
+        }
+        
+        pd.hapus(px);
+    }
+
+    private static void tampilkanPeserta(Peserta px) {
         System.out.println("ID : "+px.getId());
         System.out.println("Kode : "+px.getKode());
         System.out.println("Nama : "+px.getNama());
