02a62a344b292dd60bc42325f989960e9cc55afa
cikiprik <cikiprik@gmail.com>
2014-09-09 12:59:49 +0700

    Update set get

---
+++ aplikasi-training-management/src/main/java/com/artivisi/training/dao/OpsiDao.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.artivisi.training.dao;
+
+import com.artivisi.training.domain.Opsi;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author CEISA-DJBC
+ */
+public class OpsiDao {
+    public void simpan(Opsi o) {
+        
+    }
+    
+    public void hapus(Opsi o){
+        
+    }
+    
+    public Opsi cariById(Integer id) {
+        return null;
+    }
+    
+    public List<Opsi> cariSemua() {
+        List<Opsi> hasil = new ArrayList<>();
+        
+        return hasil;
+    }
+    
+}
