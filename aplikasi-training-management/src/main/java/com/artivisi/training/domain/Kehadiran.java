53ae31542147ede392b811e480b65244e35c851a
ra3.rachma@gmail.com <Ra3Rachma@gmail.com>
2014-09-09 10:09:59 +0700

    Kehadiran

---
+++ aplikasi-training-management/src/main/java/com/artivisi/training/domain/Kehadiran.java
@@ -0,0 +1,14 @@
+package com.artivisi.training.domain;
+
+/**
+ *
+ * @author CEISA12_2
+ */
+import java.util.Date;
+
+public class Kehadiran {
+    private Integer id;
+private Date tanggal;
+private Integer idPeserta;
+private Integer idBatch;
+}
