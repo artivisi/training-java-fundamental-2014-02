79e6b453ae60680c2cdf27367095212b8d0d86aa
ra3.rachma@gmail.com <Ra3Rachma@gmail.com>
2014-09-09 13:39:12 +0700

    qwerty

---
+++ aplikasi-training-management/src/main/java/com/artivisi/training/dao/UjianDao.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artivisi.training.dao;
+
+import com.artivisi.training.domain.Ujian;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author CEISA12_2
+ */
+public class UjianDao {
+    public void simpan(Ujian u){
+        
+    }
+    public void hapus(Ujian u){
+        
+    }
+    public Ujian cariById(Integer id){
+        return null;
+    }
+    public List<Ujian> cariSemua(){
+        List<Ujian> hasil = new ArrayList<Ujian>();
+        return hasil;
+    }
+    
+}
